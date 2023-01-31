1b63b8d4c819775bba185fd91524ee468f117c09
Anna <anya230704@gmail.com>
2023-01-31 13:42:55 +0300

    update bin/brain-games.js, bin/pakage.json, package-lock.json, package.json and src/cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
 //Welcome to the Drain Games!
+import greetings from "../src/cli.js";
+
+greetings();
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const greetings = () => {
-  console.log('brain-games');
+export const greetings = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  console.log('Hello, ${name}!');
+  console.log(`Hello, ${name}!`);
   
-  process.exit(0);
+  return name;
 }
