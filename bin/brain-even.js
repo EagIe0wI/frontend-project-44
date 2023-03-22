3f1f0b6c9e4614b838738f6355b93717ff521a89
Anna <anya230704@gmail.com>
2023-03-22 13:36:50 +0300

    updated: bin/brain-even.js. added: src/games/even.js.

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,17 +1,5 @@
-import { greetings } from '../src/cli.js';
-import { answerForm, checkAnswer } from '../src/index.js';
-import { randomNumbers } from '../src/math.js';
+#!/usr/bin/env node
 
-export const parityCheck = () => {
-  const userName = greetings('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+import { even } from '../src/games/even.js';
 
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber = randomNumbers();
-    const userAnswer = answerForm(randomNumber, 'string');
-    const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
-    if (!checkAnswer(userAnswer, correctAnswer, userName, i)) break;
-  }
-};
-parityCheck();
-export default parityCheck;
+even();
