5232a2b65cf6c7200866565f0234c580a5212a10
Anna <anya230704@gmail.com>
2023-03-22 13:54:52 +0300

    updated bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,39 +1,5 @@
-import { greetings } from '../src/cli.js';
-import { answerForm, checkAnswer } from '../src/index.js';
-import { calculator, getOperator, randomNumbers } from '../src/math.js'
+#!/usr/bin/env node
 
-export const calc = () => {
-  const userName = greetings('What is the result of the expression?');
-  console.log('What is the result of the expression?');
+import calc from '../src/games/even.js';
 
-  for (let i = 0; i < 3; i += 1) {
-
-    const operator = getOperator(['+', '-', '*']);
-
-    const number1 = randomNumbers();
-    const number2 = randomNumbers();
-
-    const example = `${number1} ${operator} ${number2}`;
-    const userAnswer = answerForm(example);
-    let correctAnswer = 0;
-
-    switch (operator) {
-      case '+':
-        correctAnswer = calculator(number1, number2, '+');
-        break;
-
-      case '-':
-        correctAnswer = calculator(number1, number2, '-');
-        break;
-
-      case '*':
-        correctAnswer = calculator(number1, number2, '*');
-        break;
-      
-      default:
-    }
-    if (!checkAnswer(userAnswer, correctAnswer, userName, i)) break;
-  }
-}
 calc();
-export default calc;
