ea64613a36f79b52a05b4bc1f75913448d0bcb4d
Anna <anya230704@gmail.com>
2023-03-22 13:54:26 +0300

    added src/games/calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,39 @@
+import { greetings } from '../src/cli.js';
+import { answerForm, checkAnswer } from '../src/index.js';
+import { calculator, getOperator, randomNumbers } from '../src/math.js'
+
+export const calc = () => {
+  const userName = greetings();
+  console.log('What is the result of the expression?');
+
+  for (let i = 0; i < 3; i += 1) {
+
+    const operator = getOperator(['+', '-', '*']);
+
+    const number1 = randomNumbers();
+    const number2 = randomNumbers();
+
+    const example = `${number1} ${operator} ${number2}`;
+    const userAnswer = answerForm(example);
+    let correctAnswer = 0;
+
+    switch (operator) {
+      case '+':
+        correctAnswer = calculator(number1, number2, '+');
+        break;
+
+      case '-':
+        correctAnswer = calculator(number1, number2, '-');
+        break;
+
+      case '*':
+        correctAnswer = calculator(number1, number2, '*');
+        break;
+      
+      default:
+    }
+    if (!checkAnswer(userAnswer, correctAnswer, userName, i)) break;
+  }
+}
+calc();
+export default calc;
