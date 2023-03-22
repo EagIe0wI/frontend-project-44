140bfbef56a924c36095d3ffa9de00c1146876cc
Anna <anya230704@gmail.com>
2023-03-22 13:58:34 +0300

    update src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,34 +3,34 @@ import { answerForm, checkAnswer } from '../src/index.js';
 import { calculator, getOperator, randomNumbers } from '../src/math.js'
 
 export const calc = () => {
-  const userName = greetings();
-  console.log('What is the result of the expression?');
+    console.log('What is the result of the expression?');
+    const userName = greetings();
 
-  for (let i = 0; i < 3; i += 1) {
+    for (let i = 0; i < 3; i += 1) {
 
-    const operator = getOperator(['+', '-', '*']);
+        const operator = getOperator(['+', '-', '*']);
 
-    const number1 = randomNumbers();
-    const number2 = randomNumbers();
+        const number1 = randomNumbers();
+        const number2 = randomNumbers();
 
-    const example = `${number1} ${operator} ${number2}`;
-    const userAnswer = answerForm(example);
-    let correctAnswer = 0;
+        const example = `${number1} ${operator} ${number2}`;
+        const userAnswer = answerForm(example);
+        let correctAnswer = 0;
 
-    switch (operator) {
-      case '+':
-        correctAnswer = calculator(number1, number2, '+');
-        break;
+        switch (operator) {
+        case '+':
+            correctAnswer = calculator(number1, number2, '+');
+            break;
 
-      case '-':
-        correctAnswer = calculator(number1, number2, '-');
-        break;
+        case '-':
+            correctAnswer = calculator(number1, number2, '-');
+            break;
 
-      case '*':
-        correctAnswer = calculator(number1, number2, '*');
-        break;
+        case '*':
+            correctAnswer = calculator(number1, number2, '*');
+            break;
       
-      default:
+        default:
     }
     if (!checkAnswer(userAnswer, correctAnswer, userName, i)) break;
   }
