f58dcdb15bc53ed47034c7e66eab7cc8458bd0cb
Anna <anya230704@gmail.com>
2023-03-23 10:06:53 +0300

    update bin/brain-gcd.js. added src/games/gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,21 +1,5 @@
-import { greetings } from '../src/cli.js';
-import { answerForm, checkAnswer } from '../src/index.js';
-import { getDivisor, randomNumbers } from '../src/math.js';
+#!/usr/bin/env node
 
-export const gcd = () => {
-    const userName = greetings('Find the greatest common divisor of given numbers.');
-    console.log('Find the greatest common divisor of given numbers.');
-
-  for (let i = 0; i < 3; i += 1) {
-    let number1 = randomNumbers();
-    let number2 = randomNumbers();
-    const ex = `${number1} ${number2}`;
-    const userAnswer = answerForm(ex);
-    const correctAnswer = getDivisor(number1, number2);
-
-    if (!checkAnswer(userAnswer, correctAnswer, userName, i)) break;
-  }
-};
+import gcd from '../src/games/gcd.js';
 
 gcd();
-export default gcd;
